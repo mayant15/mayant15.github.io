9024560f9fd688baddd38e7d55b9bff7c376243c
Mayant Mukul <mayantmukul@hotmail.com>
2020-04-06 23:28:01 +0530

    Clean the site, remove all caching js, remove analytics

--- _scripts/main.js
+++
@@ -1,7 +0,0 @@
-import './web-font-loader';
-import { checkExternalLinks, startProgressBar } from './helpers';
-
-window.addEventListener('load', () => {
-  checkExternalLinks(document);
-  startProgressBar(document);
-}, false);
